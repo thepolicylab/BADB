c2b7a6701d5f99e101e83205571d26ee315fbe35
Edward Huh <edward_huh@brown.edu>
2020-06-17 11:46:00 -0400

    updating branch

--- badb/data_utils.py
+++ badb/data_utils.py
@@ -1,14 +1,12 @@
 import json
 from pathlib import Path
 
+
 ROOT_DIR = Path(__file__).parent.parent
 
-
 def collect_dict(ss_obj):
-    init_list = list(vars(ss_obj))[:-3]
+    unwanted_keys = ['components', 'metadata', 'analysis']
+    init_list = [e for e in list(vars(ss_obj)) if e not in unwanted_keys]
     init_dict = dict(map(lambda k: (k, vars(ss_obj).get(k, None)), init_list))
-    meta_dict = vars(ss_obj.metadata)
-    anal_dict = vars(ss_obj.analysis)
-    comp_dict = vars(ss_obj.components)
-    temp = {**init_dict, **meta_dict, **anal_dict, **comp_dict}
+    temp = {**init_dict, **vars(ss_obj.metadata), **vars(ss_obj.analysis), **vars(ss_obj.components)}
     return json.dumps({str(k): str(v) for k, v in temp.items()})
